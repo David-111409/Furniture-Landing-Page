11845120d5fc13310e383de4f220347239848f02
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2026-04-27 20:46:18 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Bird } from "lucide-react";
+import { X } from "lucide-react";
+
+function App() {
+    return (
+        <div className="flex gap-4">
+            <Bird />
+            <X />
+        </div>
+    );
+}
+
+export default App;
